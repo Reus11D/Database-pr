9d2cdd47b631ac16a6bf07bdb45c32774776b38a
Abaiuly Abilhaiyr <abilhayir.abay@gmail.com>
2021-02-10 10:48:21 +0600

    Php and txt

---
+++ first_file.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web - programming</title>
+</head>
+<body>
+    <?php
+      $number1 = 12;
+      $number2 = 14;
+      $sum = $number1 + $number2;
+      return $sum;
+     ?>
+</body>
+</html>

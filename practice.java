7fb4c73c6b8260d5ac99ea51dc8584c3984201c4
Reus11D <75592882+Reus11D@users.noreply.github.com>
2021-02-10 11:16:10 +0600

    mod of number A

---
+++ practice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main{
+   public static void main(String[] args){
+   int a = 22;
+   in b = a % 10;
+   int c = a / 10;
+   System.out.println(c + b);
+   
+   }
+  }
